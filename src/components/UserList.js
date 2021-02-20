c533ef6bba16fd66d3b61f02e49c1a2038fba800
Irvin Meneses <developer@irvinemmanuel.xyz>
2021-02-20 00:40:47 -0500

    first commit

---
+++ src/components/UserList.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useContext } from 'react';
+import UserContext from '../context/Users/UserContext'
+
+const UserList = () => {
+
+  const { users, getUsers, getProfile } = useContext(UserContext)
+
+  useEffect(() => {
+    getUsers()
+  }, [])
+  
+  return (
+    <div className="list-group h-100">
+      {
+        users.map(user => (
+          <a className='list-group-item list-group-item-action d-flex flex-row justify-content-start' href='#!' key={user.id} onClick={() => getProfile(user.id)}>
+            <img className='img-fluid mr-4 rounded-circle' width='70' src={user.avatar} alt="Avatar"/>
+            <p>{`${user.first_name} ${user.last_name}`}</p>
+          </a>
+        ))
+      }
+    </div>
+  )
+}
+
+export default UserList;
